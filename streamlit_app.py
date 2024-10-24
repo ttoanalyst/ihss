4a93ccf1d551405f5440133f365528ffa3e0af19
Sisay Menji <50823318+ttoanalyst@users.noreply.github.com>
2024-10-24 11:37:59 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st 
 import plotly.graph_objects as go, plotly.express as px
-# from .. import prepare_data as pd 
+# from prepare_data import prepare_data as pd 
 import sys
 import os
 # Get the parent directory path
